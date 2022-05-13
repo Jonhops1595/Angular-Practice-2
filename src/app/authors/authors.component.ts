3f364c298718691398084e51f34ab9bf40f0882a
Jon Hopkins <jonhops2@gmail.com>
2022-05-13 17:23:32 -0400

    End of Day 1

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorsService } from '../authors.service';
+
+@Component({
+  selector: 'authors',
+  template: `
+  <h2>{{ title }}</h2>
+  <ul>
+      <li *ngFor="let author of authors">
+          {{ author }}
+      </li>
+  <ul>
+  `
+})
+
+
+export class AuthorsComponent implements OnInit {
+  title;
+  authors;
+  numAuthors;
+
+  constructor(service: AuthorsService) { 
+    this.authors = service.getAuthors();
+    this.numAuthors = service.getAuthors().length;
+    this.title = this.numAuthors + " Authors"
+  }
+
+  ngOnInit(): void {
+  }
+
+}
